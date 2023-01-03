e12d1e478344225d0a40bc95ccc08514012605dc
jain <janepark@webiznet.com>
2023-01-03 17:32:33 +0900

    sdf

---
+++ src/main/java/Adapte/AdapterImpl.java
@@ -0,0 +1,16 @@
+package Adapte;
+
+public class AdapterImpl implements Adapter{
+    //Math math;
+
+    @Override
+    public Float twiceOf(Float f){
+
+    return (float) Math.twoTime(f.doubleValue());
+    }
+
+    @Override
+    public Float halfOf(Float f) {
+        return null;
+    }
+}
