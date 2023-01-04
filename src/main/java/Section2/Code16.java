1c331d9463db339e2849530d65c480d78848bc53
jain <janepark@webiznet.com>
2023-01-04 09:47:40 +0900

    sdfsdf

---
+++ src/main/java/Section2/Code16.java
@@ -0,0 +1,15 @@
+package Section2;
+
+import java.util.Scanner;
+
+public class Code16 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int a = kb.nextInt();
+        int b = kb.nextInt();
+
+        power(a,b);
+
+        kb.close();
+    }
+}
