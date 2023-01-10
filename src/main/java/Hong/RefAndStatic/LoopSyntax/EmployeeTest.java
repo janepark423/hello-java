52f66a4be06b43b1154f43fade08caae75fd78b2
jain <janepark@webiznet.com>
2023-01-10 16:13:27 +0900

    생성자 레퍼런스와 static (example: Employee)

--- src/main/java/Hong/RefAndStatic/LoopSyntax/EmployeeTest.java
+++ src/main/java/Hong/RefAndStatic/LoopSyntax/EmployeeTest.java
@@ -3,11 +3,14 @@ package Hong.RefAndStatic.LoopSyntax;
 public class EmployeeTest {
     public static void main(String[] args) {
         int[] hours0 = {2, 4, 3, 4, 5, 8, 8};
+        int[] hours1 = {7, 3, 4, 3, 3, 4, 4};
 
         //객체 생성
         Employee e0 = new Employee("직원0", hours0);
+        Employee e1 = new Employee("직원1", hours1);
         //객체 배열 만듦
-        Employee[] employees = {e0};
+        Employee[] employees = {e0, e1};
+        //정보출력
         for (int i = 0; i < employees.length; i++) {
             employees[i].printTotalHours();
         }
