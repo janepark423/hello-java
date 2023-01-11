23a9e3d3ab7681fa3858587d83332c8179672376
jain <janepark@webiznet.com>
2023-01-11 15:26:17 +0900

    게터와 세터

--- src/main/java/Hong/GetterNSetter/PersonTest.java
+++ src/main/java/Hong/GetterNSetter/PersonTest.java
@@ -5,8 +5,8 @@ public class PersonTest {
 
         Person park = new Person("쟈인", "010-xxxx-xxxx");
         //게터를 호출하여 필드값을 얻으시오
-        System.out.printf("이름 : %s\n", park.name);
-        System.out.printf("dusfkrcj : %s\n", park.phoneNumber);
+        System.out.printf("이름 : %s\n", park.getName());
+        System.out.printf("연락처 : %s\n", park.getPhoneNumber());
     }
 }
 
@@ -14,9 +14,18 @@ class Person {
     private String name;
     private String phoneNumber;
 
+    //생성자
     public Person(String n, String p) {
         name = n;
         phoneNumber = p;
     }
+
     //게터를 추가하시오
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 }
